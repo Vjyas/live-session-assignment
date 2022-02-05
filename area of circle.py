d16895967aa6fea06e839fc5dc382565433dbb35
Vjyas <95133861+Vjyas@users.noreply.github.com>
2022-02-05 17:25:01 +0530

    Update area of circle.py

--- area of circle.py
+++ area of circle.py
@@ -1,3 +1,9 @@
 print('Enter the radius of the circle:')
 a=float(input())
 print('Area of circle with radius 1.1 is:',3.14*a*a)
+
+
+print('enter filename:')
+fn=input()
+f=fn.split('.')
+print('extension of file is:'+f[-1])
